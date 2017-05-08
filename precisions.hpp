2374d9c9bb8639bde4cf3e1f68bc23b6896f6278
Peter Labus <plabus@sissa.it>
2017-05-08 18:28:54 +0200

    Enum for Floating Point Types.

---
+++ precisions.hpp
@@ -0,0 +1,20 @@
+
+/**
+    NCG++
+    precisions.hpp
+
+    Purpose:
+    Declare all available precisons.
+
+    @author Peter Labus
+    @version 0.1
+    08.05.2017
+*/
+
+#pragma once
+
+enum class FloatingPointPrecision
+{
+  Single,
+  Double,
+};
