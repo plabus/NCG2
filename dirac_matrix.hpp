173924e654deb5e2ca5d16772a8536ddfbb9beb9
Peter Labus <plabus@sissa.it>
2017-05-09 12:23:55 +0200

    Added member variables needed for the Dirac Matrix.

--- dirac_matrix.hpp
+++ dirac_matrix.hpp
@@ -3,8 +3,8 @@
     dirac_matrix.hpp
 
     Purpose:
-    Provide a container/wrapper for the Dirac
-    Matrix in terms of hermitian and anti-hermitian
+    Provide a container/wrapper for the Dirac Matrix
+    in terms of hermitian and anti-hermitian
     matrices.
 
     @author Peter Labus
@@ -21,4 +21,15 @@ class DiracMatrix
 
     virtual FT operator()(int row, int column) const;
     virtual FT operator()(int row, int column);
+
+
+  private:
+
+    const MathLibrary matrix_impl_;
+    const ModelParameters pqn_;
+    const CliffordAlgebra cliff_;
+    const int num_H_;
+    const int num_L_;
+    std::vector< HermitianMatrix > H_;
+    std::vector< AntiHermitianMatrix > L_;
 };
