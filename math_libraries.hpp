26e4850b16d6a1ec62f741b808960e8568e42e06
Peter Labus <plabus@sissa.it>
2017-05-09 12:24:22 +0200

    List all available implementations for the little matrices.

---
+++ math_libraries.hpp
@@ -0,0 +1,20 @@
+/**
+    NCG++
+    math_libraries.hpp
+
+    Purpose:
+    Lists all available implementations
+    for the little matrices and functions
+    acting on them.
+
+    @author Peter Labus
+    @version 0.1
+    09.05.2017
+*/
+
+#pragma once
+
+enum class MathLibrary
+{
+  Custom,
+};
