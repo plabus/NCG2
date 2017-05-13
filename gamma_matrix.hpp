a438ea22ff33b2d00a6a759963fa00718e05a3e3
Peter Labus <plabus@sissa.it>
2017-05-13 11:46:25 +0200

    Added documentation to GammaMatrix class.

--- gamma_matrix.hpp
+++ gamma_matrix.hpp
@@ -18,9 +18,19 @@
 #include <complex>
 
 
+
+//===================//
+//                   //
+//    GammaMatrix    //
+//                   //
+//===================//
+
 class GammaMatrix
 {
   public:
+
+    // Public Member Functions:
+    // ========================
 
     // Constructors:
     GammaMatrix(void) = delete;
@@ -28,18 +38,31 @@ class GammaMatrix
     explicit GammaMatrix(std::initializer_list< std::complex<int> > const& list);
     GammaMatrix& operator=(GammaMatrix const& other);
 
-    std::complex<int> operator()(const int row, const int col) const { return M_[row*size_+col]; }
-    std::complex<int>& operator()(const int row, const int col) { return M_[row*size_+col]; }
+    // Assigment Operators:
     GammaMatrix& operator*=(GammaMatrix const& other);
     GammaMatrix& operator*=(std::complex<int> c);
-    GammaMatrix operator-(void) const;
+
+    // Logical Operators:
     bool operator==(GammaMatrix const& other) const;
+
+    // Miscellaneous:
+    GammaMatrix operator-(void) const; //!< Return the negative of this as a new GammaMatrix
+
+    // Getters:
+    std::complex<int> operator()(const int row, const int col) const { return M_[row*size_+col]; }
+    int size(void) const { return size_; }
+
+    // Setters:
+    std::complex<int>& operator()(const int row, const int col) { return M_[row*size_+col]; }
+
 
     friend std::ostream& operator<<(std::ostream& os, GammaMatrix const& A);
 
-    int size(void) const { return size_; }
 
   private:
+
+    // Private Member Variables:
+    // =========================
 
     const int size_;
     std::vector< std::complex<int>  > M_;
@@ -47,21 +70,36 @@ class GammaMatrix
     void print_(void) const;
 };
 
+
+// Non-Member Functions:
+// =====================
+
 // These are addition, substraction, multiplication and outer (tensor) multiplication
 GammaMatrix operator+(GammaMatrix const& A, GammaMatrix const& B);
 GammaMatrix operator-(GammaMatrix const& A, GammaMatrix const& B);
 GammaMatrix operator*(GammaMatrix const& A, GammaMatrix const& B);
-GammaMatrix operator*(std::complex<int> c, GammaMatrix const& A);
-GammaMatrix operator/(GammaMatrix const& A, int c);
 GammaMatrix operator%(GammaMatrix const& A, GammaMatrix const& B);
 
+// These allow to multiply a GammaMatrix with a complex integer
+// and divide by an integer (integer divison)
+GammaMatrix operator*(std::complex<int> c, GammaMatrix const& A);
+GammaMatrix operator/(GammaMatrix const& A, int c);
+
+// These are commutators and anticommutators of two GammaMatrices
 GammaMatrix commutator(GammaMatrix const& A, GammaMatrix const& B);
 GammaMatrix anticommutator(GammaMatrix const& A, GammaMatrix const& B);
 
+// These return true is M is hermitain / antihermitian respectively
 bool is_hermitian(GammaMatrix const& M);
 bool is_antihermitian(GammaMatrix const& M);
 
 
+
+//=====================//
+//                     //
+//    PauliMatrices    //
+//                     //
+//=====================//
 
 struct PauliMatrices
 {
@@ -71,4 +109,5 @@ struct PauliMatrices
   const GammaMatrix sigma3;
 };
 
+// Generates a d x d unity (gamma) matrix
 GammaMatrix Unity(const int d);
